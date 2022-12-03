54b60fda3611c7f485eb2bd1bec5fe91e93f2a6e
Martin老師 <tpemartin@gmail.com>
2022-12-03 08:47:54 +0800

    update

--- examples/create-react-app-with-styled-components-typescript/src/index.tsx
+++ examples/create-react-app-with-styled-components-typescript/src/index.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
-import ReactDOM from 'react-dom';
 import CssBaseline from '@mui/material/CssBaseline';
 import App from './App';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
-  <React.Fragment>
-    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-    <CssBaseline />
-    <App />
-  </React.Fragment>,
-  document.getElementById('root'),
-);
+const container = document.getElementById('root');
+const root = createRoot(container!) //if you use TypeScript
+root.render(
+<React.Fragment>
+  {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+  <CssBaseline />
+  <App />
+</React.Fragment>);
